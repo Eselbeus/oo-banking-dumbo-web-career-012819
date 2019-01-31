19c0b7e1796ca743b61d96bfde03df00cdef3ea1
eselbeus <eselbeus@gmail.com>
2019-01-31 23:38:40 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,13 +2,11 @@ require 'pry'
 class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
-
   
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
-    
   end
   
   def deposit(money)
