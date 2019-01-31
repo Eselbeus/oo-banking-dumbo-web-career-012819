51fffc2e0c0693bebf0f17b749eb253d24205cdf
eselbeus <eselbeus@gmail.com>
2019-01-31 23:18:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,6 +22,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+      "Transaction rejected. Please check your account balance"
     else
       return "Transaction rejected. Please check your account balance"
     end
