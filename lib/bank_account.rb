7727ec54b06cc25c7fcb756ce0b1b948f6e69e53
eselbeus <eselbeus@gmail.com>
2019-01-31 22:32:49 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,32 @@
 class BankAccount
+  attr_accessor :name, :balance
+
   
   def initialize(name)
     @name = name
+    @balance = 1000
+    @status = "open"
     
   end
   
+  def deposit(money)
+    @balance += money
+    @balance
+  end
+  
+  def display_balance(@balance) 
+    @balance
+  end
+  
+  def valid? 
+    if @balance > 0 && @status == "open"
+      return true
+    else 
+      return false 
+    end
+  end
+  
+  def close_account(@name)
+    @status == "closed"
+  end
 end
